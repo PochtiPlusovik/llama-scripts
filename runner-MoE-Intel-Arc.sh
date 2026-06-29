6da21545c9231fa94478ac0037a6135817499467
Ryzen 5600kg <162839557+PochtiPlusovik@users.noreply.github.com>
2026-06-29 12:22:30 +1000

    Update runner-MoE-Intel-Arc.sh

--- runner-MoE-Intel-Arc.sh
+++ runner-MoE-Intel-Arc.sh
@@ -1,8 +1,11 @@
 #!/bin/sh 
 cd ~/llama.cpp
 source /opt/intel/oneapi/setvars.sh # Comment out if you don't use SYCL
-~/llama.cpp/build/bin/llama-server    -m /path/to/model.gguf \ # Replace with the path to you model 
-	--no-mmap \ 
+# Replace with the path to you model
+# You can change the comtext window size
+# You can also change the port a convenient one
+~/llama.cpp/build/bin/llama-server    -m /path/to/model.gguf \
+	--no-mmap \
 	-np 1 \
-	-c 128000 \ # You can change the comtext window size
-	--port 8080 \ # You can also change the port a convenient one
+	-c 128000 \
+	--port 8080 \
